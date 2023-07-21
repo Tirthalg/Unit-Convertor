18f30cec9e29285f669217df4398e171245c5bed
Tirthal <88190817+Tirthalg@users.noreply.github.com>
2023-07-21 21:21:43 +0530

    final

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import data from './data.json' assert {type:'json'};
-const type = new URLSearchParams(window.location.search).get('type');if(type !== null) document.title = type;
+const type = new URLSearchParams(window.location.search).get('type');
+if(type !== null) document.title = type;
+else    window.location.href = "./convertor.html?type=Weight and Mass";
 
 $(document).ready(function(){
     $("#h").text(type+" Convertor")
@@ -11,13 +13,17 @@ $(document).ready(function(){
     });
 // --------------------------------Index href---------------------------------------
     var current = [];
+    var map = [];
 // ---------------------------------Sidebar-----------------------------------------
     var html =  '<div onclick="nclose()" name="close" id="close" class="item">'+
                 '    <i class="fas fa-xmark"></i> '+
                 '    Close'+
                 '</div>'
     data.forEach(element => {
-        if(type === element['name'])    current = element['units'];
+        if(type === element['name']){
+            current =element['units']; 
+            map = element['multiplier'];
+        }
         html = html.concat( '<div class="item" name="'+element['name'] +'">   <i class="fas '+element['icon'] +'"></i>'+ element['name'] +'</div>');
     });
     $(".sidebar").html(html);
@@ -33,20 +39,59 @@ $(document).ready(function(){
 // ------------------------------Sidebar href---------------------------------------
 
 // ---------------------------------Options-----------------------------------------
+    var count =0;
     current.forEach(element => {
         $("#unit1").append(`<option value="${element}">
                                 ${element}
                             </option>`);
 
         $("#outputs").append(`
-            <div class="card small column" name="${element}">${element} = </div>
+            <div class="card small column" name="${element}" id="${count++}">${element} = </div>
         `)
 
 
     });
 // ---------------------------------Options-----------------------------------------
-    
 
+    var text = [];
+    var currmap = map[current[0]]
+    for(var i=0;i<current.length;i++){
+        text.push($("#"+i).text());
+    };
+  
+    function changed(unit,value){
+        currmap = map[unit];
+        if(type==="Temperature"){
+            for(var i=0;i<current.length;i++){
+                $("#"+i).text(text[i]+(value*currmap[current[i]]['mul']+currmap[current[i]]['add']));
+            };
+        }
+        else{
+            for(var i=0;i<current.length;i++){
+                $("#"+i).text(text[i]+value*currmap[current[i]]);
+            };
+        }
+    }
+    var selected = $("#unit1").find(':selected').val();
+    var value = $("#amount1").val();
+    changed(selected,value);
+    $("#unit1").on( "change", function() {
+        selected = $(this).find(':selected').val();
+        changed(selected,value)
+    });
+    $("#amount1").on( "input", function() {
+        value = $(this).val();
+        changed(selected,value);
+    });
+
+// -------------------------------copy option---------------------------------------
+    $(".small").click(function(e){
+        var idClicked = e.target.id;
+        navigator.clipboard.writeText($("#"+idClicked).text());
+        $("#flash").show().delay(200).fadeOut();
+    });
+
+// -------------------------------copy option---------------------------------------
 
 
 });
